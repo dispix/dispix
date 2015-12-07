112e028ea33ffbc66704a30cb82ef26f28a59f8a
dispix <dispyx@gmail.com>
2015-12-08 00:31:07 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app		= express()
+var jade	= require('jade')
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static('public'))
+app.get('/', function(req, res)
+{
+	var time = new Date()
+	console.log(time.toUTCString() + ' : Get request')
+	res.render('skel.jade')
+	res.end()
+})
+app.listen(8080)
