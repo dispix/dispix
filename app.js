c1edfd9c4d245a0f94a4ddc2338e48d319ba3e76
dispix <dispyx@gmail.com>
2015-12-08 20:05:19 +0100

    App

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var express = require('express')
 var app		= express()
 var jade	= require('jade')
-var mysql 	= require('mysql')
-var db 		= mysql.createConnection({host : 'localhost', user : 'pma', password : '', database : 'test'}))
-
-db.connect()
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
