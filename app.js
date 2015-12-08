12873f30212acf3f7149e249ca5977683a7310d1
dispix <dispyx@gmail.com>
2015-12-08 20:03:42 +0100

    Init

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express')
 var app		= express()
 var jade	= require('jade')
+var mysql 	= require('mysql')
+var db 		= mysql.createConnection({host : 'localhost', user : 'pma', password : '', database : 'test'}))
+
+db.connect()
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static('public'))
 app.get('/', function(req, res)
 {
-	var time = new Date()
-	console.log(time.toUTCString() + ' : Get request')
 	res.render('skel.jade')
 	res.end()
 })
