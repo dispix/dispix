cd07346c36a12eba40dc565a2942162e82a55ef1
dispix <dispyx@gmail.com>
2015-12-16 23:42:45 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -33,28 +33,30 @@ $(document).ready(function(){
 	$(document).scroll(function()
 	{
 		var y = $(this).scrollTop()
-		var height = $(window).height() - offset - 1
+		var height = $(window).height() - offset
 
 		if (y > height)
 		{
 			$('#navbar').css('box-shadow', 'inset 0 -3px 5px -1px rgba(51, 51, 51, .6)')
+			$('#navbar').css('z-index', '')
 		}
 		else
 		{
 			$('#navbar').css('box-shadow', 'none')
+			$('#navbar').css('z-index', '1')
 		}
 
-		// if (y >= ($('#projects').offset()['top'] - $(window).height()))
+
+		// if (y > ($('#projects').offset()['top'] - $(window).height()) && $(window).height() > 700)
 		// {
 		//
 		// 	$('#skills').addClass('locked')
-		// 	$('#skills').css('top', (offset - ($('#skills').height() - $(window).height())))
+		// 	$('#skills').css('top', offset)
 		// 	$('#projects').css('margin-top', $('#home').height() + $('#skills').height())
 		// }
 		// else
 		// {
-		// 	console.log(offset - ($('#skills').height() - $(window).height()));
-		// 	$('#skills').removeClass('blocked')
+		// 	$('#skills').removeClass('locked')
 		// 	$('#skills').css('top', '')
 		// 	$('#projects').css('margin-top', '')
 		// }
